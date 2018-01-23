5bd2b6ffcc8da2e1ee382135ba637669c25120d6
Gwen <s4ngoku@gmail.com>
2018-01-23 16:57:48 +0100

    fix gestion token

--- src/modules/auth/containers/login.component.ts
+++ src/modules/auth/containers/login.component.ts
@@ -2,13 +2,14 @@ import { Component } from "@angular/core";
 import { Router } from "@angular/router";
 import { AuthService } from "../../../shared/services/auth.service";
 import { LoginData } from "../../../shared/models/login-data.model";
+import { TokenService } from "../../../shared/services/token.service";
 
 @Component({
   selector: 'ngs-login',
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
-  constructor(private _auth: AuthService, private _router: Router) {
+  constructor(private _auth: AuthService, private _tokenService: TokenService, private _router: Router) {
     if (this._auth.isAuthenticated()) {
       this._router.navigateByUrl('/admin/dashboard');
     }
@@ -16,7 +17,7 @@ export class LoginComponent {
 
   onLogin(payload: LoginData) {
     this._auth.login(payload).subscribe(response => {
-      this._auth.setToken(response['token']);
+      this._tokenService.setToken(response['token']);
       this._router.navigateByUrl('/admin/dashboard')
     });
   }
